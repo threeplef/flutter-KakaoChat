5b9440218a1851c33fe5aaaaabd5c44800523a32
minhaneul <onlynforever@gmail.com>
2022-07-09 00:20:44 +0900

    Add: appBar UI 구현 완료

--- lib/kakaochat_main_screen.dart
+++ lib/kakaochat_main_screen.dart
@@ -6,14 +6,27 @@ class KakaoChatScreen extends StatelessWidget {
   Widget _appBar() {
     Widget _left() {
       return Expanded(
-        child: Container(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Container(color: Colors.purple, width: 40, height: 60),
-              Container(color: Colors.purpleAccent, width: 60, height: 60),
-            ],
-          ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 8.0),
+              child: Text('채팅', style: TextStyle(color: Colors.grey, fontSize: 18, fontWeight: FontWeight.bold)),
+            ),
+            Row(
+              children: [
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 8.0),
+                  child: Text('오픈채팅', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+                ),
+                Container(
+                  width: 4,
+                  height: 4,
+                  decoration: const BoxDecoration(shape: BoxShape.circle, color: Colors.redAccent),
+                ),
+              ],
+            ),
+          ],
         ),
       );
     }
@@ -21,18 +34,21 @@ class KakaoChatScreen extends StatelessWidget {
     Widget _right() {
       return Row(
         children: [
-          Container(color: Colors.red, width: 60, height: 60),
-          Container(color: Colors.yellow, width: 60, height: 60),
-          Container(color: Colors.green, width: 60, height: 60),
-          Container(color: Colors.pink, width: 60, height: 60),
+          IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
+          IconButton(onPressed: () {}, icon: const Icon(Icons.chat)),
+          IconButton(onPressed: () {}, icon: const Icon(Icons.music_note_rounded)),
+          IconButton(onPressed: () {}, icon: const Icon(Icons.settings)),
         ],
       );
     }
 
     return SafeArea(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [_left(), _right()],
+      child: SizedBox(
+        height: 60,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [_left(), _right()],
+        ),
       ),
     );
   }
